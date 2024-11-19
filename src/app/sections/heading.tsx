99d6f0e40c49b4d6b49d45a9e5e4ac5ca45d5e16
Satriya Wicaksana <satriya.wicaksana@sinarmasmining.com>
2024-11-19 17:14:17 +0700

    wip

--- src/app/sections/heading.tsx
+++ src/app/sections/heading.tsx
@@ -1,12 +1,17 @@
 "use client";
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 import MainBanner from "../components/main-banner";
 import MobileBanner from "../components/mobile-banner";
 import CATButton from "../components/cat-button";
 
 export default function Heading() {
+  const targetRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({ target: targetRef });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["100%", "-50%"]);
   return (
     <React.Fragment>
       <div className="flex-1 hidden md:grid place-items-center">
@@ -27,7 +32,6 @@ export default function Heading() {
             delay: 1.2,
           },
         }}
-        className='max-w-3xl mx-auto'
       >
         <strong>Barevisual</strong> specializes in capturing the energy and grit
         of running events, from the powerful strides to the focused
@@ -38,6 +42,41 @@ export default function Heading() {
       </motion.p>
       <div className="flex justify-center my-4">
         <CATButton>Get in touch!</CATButton>
+      </div>
+      {/* <div className="flex justify-end relative overflow-x-hidden">
+        <motion.div
+          className="flex justify-end relative -right-40 space-x-4 overflow-x-scroll no-scrollbar"
+          // initial={{ x: 1000, opacity: 0 }}
+          // animate={{
+          //   x: 600,
+          //   opacity: 1,
+          //   transition: {
+          //     type: "spring",
+          //     damping: 10,
+          //     transition: 0.5,
+          //     delay: 1.2,
+          //   },
+          // }}
+        >
+          <div className="w-52 h-80 bg-red-500" />
+          <div className="w-52 h-80 bg-orange-500" />
+          <div className="w-52 h-80 bg-yellow-500" />
+          <div className="w-52 h-80 bg-cyan-500" />
+        </motion.div>
+      </div> */}
+      <div className="bg-gray-500 h-[500vh]" ref={targetRef}>
+        <div className="sticky h-[100vh] top-0 flex items-center justify-start overflow-clip">
+          <motion.div className="flex gap-x-4 px-4" style={{ x }}>
+            <div className="w-60 h-80 bg-red-500" />
+            <div className="w-60 h-80 bg-orange-500" />
+            <div className="w-60 h-80 bg-yellow-500" />
+            <div className="w-60 h-80 bg-cyan-500" />
+            <div className="w-60 h-80 bg-red-500" />
+            <div className="w-60 h-80 bg-orange-500" />
+            <div className="w-60 h-80 bg-yellow-500" />
+            <div className="w-60 h-80 bg-cyan-500" />
+          </motion.div>
+        </div>
       </div>
     </React.Fragment>
   );
